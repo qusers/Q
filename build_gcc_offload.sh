7b5e5fdc39f07883a150a6ecb1618ff8b37bbabe
Michiel <jespersc@xs4all.nl>
2018-06-15 23:04:16 +0200

    Update build_gcc_offload.sh

--- build_gcc_offload.sh
+++ build_gcc_offload.sh
@@ -6,7 +6,9 @@
 # export LD_LIBRARY_PATH=/usr/local/gcc-7.3/lib64/
 # export LIBRARY_PATH=/usr/local/gcc-7.3/lib64/
 #
-# Get the latest version of gcc with svn ls svn://gcc.gnu.org/svn/gcc/tags/ | grep '^gcc[_-][0-9]' | sort | tail -n1
+# For the latest version of gcc, run
+# svn ls svn://gcc.gnu.org/svn/gcc/tags/ | grep '^gcc[_-][0-9]'\
+#     | sort | tail -n1
 #
 
 work_dir=$HOME/gcc-7_offload/wrk
