f48f0a397531937437c5cf7a0d043fed60237dd7
chichi148 <jespersc@xs4all.nl>
2019-02-27 14:03:23 +0100

    PGI compiler test (Windows)

--- src/qcalc.f90
+++ src/qcalc.f90
@@ -15,7 +15,22 @@
 !!  by John Marelius  
 !!  qcalc trajectory analysis main program  
 !------------------------------------------------------------------------------!
+! module mathOps
+!         contains
+!           attributes(global) subroutine saxpy(x, y, a)
+!             implicit none
+!             real :: x(:), y(:)
+!             real, value :: a
+!             integer :: i, n
+!             n = size(x)
+!             i = blockDim%x * (blockIdx%x - 1) + threadIdx%x
+!             if (i <= n) y(i) = y(i) + a*x(i)
+!           end subroutine saxpy 
+!         end module mathOps
+
 program qcalc
+      
+
   use iso_fortran_env
 
   use trj
@@ -32,6 +47,9 @@ program qcalc
   use calc_rmsf
   use calc_com_ke
   use calc_com
+!   use mathOps
+!   use cudafor
+
   !add more calculation kind modules here...
 
   implicit none
@@ -64,6 +82,23 @@ program qcalc
   integer                 :: nkinds = 0, ncalcs = 0
   type(calc_kind_type)    :: cdef(max_calc_kinds)
   type(calc_type)         :: calcs(max_calcs)
+
+  integer, parameter :: N = 40000
+  real :: x(N), y(N), a
+!   real, device :: x_d(N), y_d(N)
+!   type(dim3) :: grid, tBlock
+
+  write(*,*) 'bla'
+
+!   tBlock = dim3(256,1,1)
+!   grid = dim3(ceiling(real(N)/tBlock%x),1,1)
+
+  x = 1.0; y = 2.0; a = 2.0
+!   x_d = x
+!   y_d = y
+!   call saxpy<<<grid, tBlock>>>(x_d, y_d, a)
+!   y = y_d
+  write(*,*) 'Max error: ', maxval(abs(y-4.0))
 
 
   !register the calculation kinds
