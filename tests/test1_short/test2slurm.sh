cef690c5e75eac482ab242ba3f8fd52082a5a22a
jesperswillem <jesperswillem@gmail.com>
2020-07-24 09:57:39 +0200

    cleaning branch

--- tests/test1_short/test2slurm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Remember to change here the number of nodes to what is available in the queue
-# you're calling. Perhaps you'll also need to issue the --exclusive option.
-sbatch -n 16 -t 01:00:00 -J testqdyn run_test_nompi.sh
-
