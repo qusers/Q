6f36737e98dde0a11605167501fd176499a324de
Willem Jespers <wije@b-an01.hpc2n.umu.se>
2020-08-03 11:49:11 +0200

    Further work on qcalc module.

--- bin/qcalc.py
+++ bin/qcalc.py
@@ -7,10 +7,15 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../s
 import qcalc
 
 class Startup(object):
-    def __init__(self):
-        data = {
+    def __init__(self,top,otrj,itrj,ilib,wtraj,calc):
+        data = { 'top' : top,
+                 'otrj' : otrj,
+                 'itrj' : itrj,
+                 'ilib' : ilib,
+                 'wraj' : wtraj,
+                 'calc' : calc,
                }
-        START = qdyn.Init(data)
+        START = qcalc.Init(data)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -21,8 +26,54 @@ if __name__ == "__main__":
     parser.add_argument('--version', 
                         action='version', 
                         version='%(prog)s 0.1.0')
+    
+            
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog='Qcalc,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description = '       == Python based MD engine == ')
 
+    
+    parser.add_argument('-t', '--top',
+                        dest = "top",
+                        default = None,
+                        required = True,
+                        help = "Q topology file")
+        
+    parser.add_argument('-ot', '--otrj',
+                        dest = "otrj",
+                        default = None,
+                        help = " Output trajectory file")
+            
+    parser.add_argument('-it', '--itrj',
+                        dest = "itrj",
+                        default = None,
+                        help = " Input trajectory file")
+                
+    parser.add_argument('-il', '--ilib',
+                        dest = "ilib",
+                        default = None,
+                        help = "Library files for used topology")
+    
+    parser.add_argument("-wt", "--wtraj",
+                        default = False,
+                        action = 'store_true',
+                        help="Write trajecotry")
+    
+    parser.add_argument('-c', '--calc',
+                        dest = "calc",
+                        required = False,
+                        help = "Comma seperated list of calculations to be performed, requires a *.calc inputfile")
+    
     args = parser.parse_args()
     
-    Startup(
+    
+    
+    Startup(top = args.top,
+            otrj = args.otrj,
+            itrj = args.itrj,
+            ilib = args.ilib,
+            wtraj = args.wtraj,
+            calc = args.calc,
            )
