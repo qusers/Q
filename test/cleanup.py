f8dd53bc74b4f1c1d5e4603cf0c88c44d7f3259d
Willem Jespers <wije@b-an01.hpc2n.umu.se>
2020-08-13 14:07:00 +0200

    Adding tests for q-q interactions

--- test/cleanup.py
+++ test/cleanup.py
@@ -4,6 +4,6 @@ curdir = os.getcwd()
 
 os.chdir('1.interactions/w-w/')
 
-os.system('rm -r compare.py md0.json Q5_data topo.json')
+os.system('rm -r compare.py md0.json Q5_data topo.json TEST')
 
 os.chdir(curdir)
