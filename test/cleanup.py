8cdffc1becb5ac03cecd42ad214e3e277b5f138b
Willem Jespers <wije@b-an01.hpc2n.umu.se>
2020-08-13 15:31:30 +0200

    Adding q-q and p-p tests.

--- test/cleanup.py
+++ test/cleanup.py
@@ -2,8 +2,12 @@ import os
 
 curdir = os.getcwd()
 
+print('cleaning w-w interaction folder')
 os.chdir('1.interactions/w-w/')
-
 os.system('rm -r compare.py md0.json Q5_data topo.json TEST')
+os.chdir(curdir)
 
+print('cleaning q-q interaction folder')
+os.chdir('1.interactions/q-q/benzene')
+os.system('rm -r compare.py eq1.inp FEP1.fep Q5_data/ TEST/')
 os.chdir(curdir)
