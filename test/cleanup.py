15c5507a582301388da02320d2c5d3ab9cc43543
Willem Jespers <wije@b-an01.hpc2n.umu.se>
2020-08-14 11:32:08 +0200

    Cleaning up cleanup script.

--- test/cleanup.py
+++ test/cleanup.py
@@ -1,49 +1,14 @@
 import os
 
-curdir = os.getcwd()
+tokeep = ['testfiles.tar.gz',
+          'README.md'
+         ]
 
-print('cleaning w-w interaction folder')
-os.chdir('1.interactions/w-w/')
-os.system('rm -r compare.py md0.json Q5_data topo.json TEST')
-os.chdir(curdir)
 
-print('cleaning q-q interaction folder')
-os.chdir('1.interactions/q-q/benzene')
-os.system('rm -r compare.py benzene-vacuum.json eq1.json FEP1.json Q5_data/ TEST/')
-os.chdir(curdir)
+rootdir = os.getcwd()
+rootdir = rootdir + '/1.interactions'
 
-print('cleaning p-p interaction folder')
-os.chdir('1.interactions/p-p/benzene')
-os.system('rm -r compare.py eq1.json benzene-vacuum.json Q5_data/ TEST/')
-os.chdir(curdir)
-
-print('cleaning q-p interaction folder')
-os.chdir('1.interactions/q-p/benzene-Na/FEP_benzene/')
-os.system('rm -r Na-benzene-vacuum.json compare.py eq1.json Q5_data/ FEP1.json TEST')
-os.chdir('../')
-os.chdir('FEP_Na')
-os.system('rm -r Na-benzene-vacuum.json compare.py eq1.json Q5_data/ FEP1.json TEST')
-os.chdir(curdir)
-
-print('cleaning q-p-w interaction folder')
-os.chdir('1.interactions/q-p-w/benzene-Na/FEP_benzene/')
-os.system('rm -r Na-benzene-vacuum.json compare.py eq1.json Q5_data/ FEP1.json TEST')
-os.chdir('../')
-os.chdir('FEP_Na')
-os.system('rm -r Na-benzene-vacuum.json compare.py eq1.json Q5_data/ FEP1.json TEST')
-os.chdir(curdir)
-
-print('cleaning w-p interaction folder')
-os.chdir('1.interactions/w-p/benzene')
-os.system('rm -r compare.py eq1.json benzene-water.json Q5_data/ TEST/')
-os.chdir(curdir)
-
-print('cleaning w-q interaction folder')
-os.chdir('1.interactions/w-q/benzene')
-os.system('rm -r compare.py eq1.json benzene-water.json Q5_data/ TEST/ FEP1.json')
-os.chdir(curdir)
-
-print('cleaning w-w interaction folder')
-os.chdir('1.interactions/w-w/water')
-os.system('rm -r compare.py eq1.json water.json Q5_data/ TEST/ ')
-os.chdir(curdir)
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        if not file in tokeep:
+            os.remove(os.path.join(subdir, file))
