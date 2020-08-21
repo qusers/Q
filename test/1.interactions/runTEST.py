6c3f874feabca82a146b6b504439c594a5bdeb6b
Willem Jespers <wije@b-an01.hpc2n.umu.se>
2020-08-21 11:24:33 +0200

    Fixing runTEST.py further.

--- test/1.interactions/runTEST.py
+++ test/1.interactions/runTEST.py
@@ -2,20 +2,39 @@ import os
 import shlex
 import subprocess
 import argparse
+import sys
 
 class Startup(object):
     def __init__(self,arch):
         self.arch =   arch
+        executable = sys.executable
+        print("Checking Python version")
+        print("================================")
+        print("version is {}.{}.{}".format(sys.version_info[0],
+                                           sys.version_info[1],
+                                           sys.version_info[2],
+                                          ))
+        print("================================")
+
+        if sys.version_info[0] < 3: 
+            print("Q-GPU only works with Python 3.6 or higher")
+            print("Exiting now")
+            sys.exit()
+
+        if sys.version_info[0] == 3 and sys.version_info[1] < 6:
+            print("Q-GPU only works with Python 3.6 or higher")
+            print("Exiting now")    
+            sys.exit()   
         
-        tests = [['p-p/benzene/','python ../../../../bin/qdyn.py -t Q5_data/benzene-vacuum.top -m Q5_data/eq1.inp -d TEST -r Q5_data/'],
-                 ['q-p/benzene-Na/FEP_benzene','python ../../../../../bin/qdyn.py -t Q5_data/Na-benzene-vacuum.top -m Q5_data/eq1.inp -d TEST -f Q5_data/FEP1.fep -r Q5_data/'],
-                 ['q-p/benzene-Na/FEP_Na','python ../../../../../bin/qdyn.py -t Q5_data/Na-benzene-vacuum.top -m Q5_data/eq1.inp -d TEST -f Q5_data/FEP1.fep -r Q5_data/'],
-                 ['q-p-w/benzene-Na/FEP_benzene','python ../../../../../bin/qdyn.py -t Q5_data/Na-benzene-water.top -m Q5_data/eq1.inp -d TEST -f Q5_data/FEP1.fep -r Q5_data/'],
-                 ['q-p-w/benzene-Na/FEP_Na','python ../../../../../bin/qdyn.py -t Q5_data/Na-benzene-water.top -m Q5_data/eq1.inp -d TEST -f Q5_data/FEP1.fep -r Q5_data/'],
-                 ['q-q/benzene/','python ../../../../bin/qdyn.py -t Q5_data/benzene-vacuum.top -m Q5_data/eq1.inp -d TEST -r Q5_data -f Q5_data/FEP1.fep'],
-                 ['w-p/benzene/','python ../../../../bin/qdyn.py -t Q5_data/benzene-water.top -m Q5_data/eq1.inp -d TEST -r Q5_data/'],
-                 ['w-q/benzene/','python ../../../../bin/qdyn.py -t Q5_data/benzene-water.top -m Q5_data/eq1.inp -f Q5_data/FEP1.fep -d TEST -r Q5_data/'],
-                 ['w-w/water/','python ../../../../bin/qdyn.py -t Q5_data/water.top -m Q5_data/eq1.inp -d TEST -r Q5_data/']]
+        tests = [['p-p/benzene/',executable, '../../../../bin/qdyn.py -t Q5_data/benzene-vacuum.top -m Q5_data/eq1.inp -d TEST -r Q5_data/'],
+                 ['q-p/benzene-Na/FEP_benzene',executable, '../../../../../bin/qdyn.py -t Q5_data/Na-benzene-vacuum.top -m Q5_data/eq1.inp -d TEST -f Q5_data/FEP1.fep -r Q5_data/'],
+                 ['q-p/benzene-Na/FEP_Na',executable, '../../../../../bin/qdyn.py -t Q5_data/Na-benzene-vacuum.top -m Q5_data/eq1.inp -d TEST -f Q5_data/FEP1.fep -r Q5_data/'],
+                 ['q-p-w/benzene-Na/FEP_benzene',executable, '../../../../../bin/qdyn.py -t Q5_data/Na-benzene-water.top -m Q5_data/eq1.inp -d TEST -f Q5_data/FEP1.fep -r Q5_data/'],
+                 ['q-p-w/benzene-Na/FEP_Na',executable, '../../../../../bin/qdyn.py -t Q5_data/Na-benzene-water.top -m Q5_data/eq1.inp -d TEST -f Q5_data/FEP1.fep -r Q5_data/'],
+                 ['q-q/benzene/',executable, '../../../../bin/qdyn.py -t Q5_data/benzene-vacuum.top -m Q5_data/eq1.inp -d TEST -r Q5_data -f Q5_data/FEP1.fep'],
+                 ['w-p/benzene/',executable, '../../../../bin/qdyn.py -t Q5_data/benzene-water.top -m Q5_data/eq1.inp -d TEST -r Q5_data/'],
+                 ['w-q/benzene/',executable, '../../../../bin/qdyn.py -t Q5_data/benzene-water.top -m Q5_data/eq1.inp -f Q5_data/FEP1.fep -d TEST -r Q5_data/'],
+                 ['w-w/water/',executable, '../../../../bin/qdyn.py -t Q5_data/water.top -m Q5_data/eq1.inp -d TEST -r Q5_data/']]
         
         self.curdir = os.getcwd()
 
@@ -24,7 +43,8 @@ class Startup(object):
             print("Running test: {}".format(test[0].split('/')[0]))
             os.chdir(test[0])
             subprocess.check_output(['tar', '-xvf', 'testfiles.tar.gz'])
-            args = shlex.split(test[1])
+            args = shlex.split(test[2])
+            args = args + [test[1]]
             if self.arch == 'gpu':
                 args.append('--gpu')
             print(' '.join(args))
@@ -34,7 +54,6 @@ class Startup(object):
             print("======================================================")    
             os.system('rm -r TEST Q5_data compare.py')
             os.chdir(self.curdir)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
