7e36fb5ecb0817273fb5a443e4b72da513ad682c
David Araripe <d.figueiredo.vidal@lacdr.leidenuniv.nl>
2023-11-14 17:19:04 +0100

    update cli for qmapfep

---
+++ src/QligFEP/CLI/qmapfep_cli.py
@@ -0,0 +1,46 @@
+import argparse
+from QligFEP.qmapfep import Init
+
+class Startup(object):
+    """
+    Create dual topology FEP files based on two ligands
+    """
+    def __init__(self, data, *args, **kwargs):
+        START = Init(data)
+        
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        prog='QmapFEP',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description='FEP map generator based on selected distance metrics.')
+    parser.add_argument('-i', '--insdf',
+                        dest="isdf",
+                        required=True,
+                        help=".sdf file name")
+    parser.add_argument('-m', '--metric',
+                        dest="metric",
+                        default='MFP',
+                        choices=['MFP', 'Tanimoto', 'MCS', 'SMILES'],
+                        required=False,
+                        help="Distance metric for ligand pairwairse comparison")
+    parser.add_argument('-o', '--otxt',
+                        dest="o",
+                        required=True,
+                        help="Name for output file")
+
+    parser.add_argument('-wd', '--workdir',
+                        dest="wd",
+                        default='workdir',
+                        help="Name for the working directory")                        
+    
+    return parser.parse_args()
+    
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    parser = argparse.ArgumentParser(
+        prog='QmapFEP',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description='FEP map generator based on selected distance metrics.')
+
+    Startup(vars(args))
