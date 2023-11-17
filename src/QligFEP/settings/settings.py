ae0f721163153461246e474d0c81a072ce2b9458
David Araripe <d.figueiredo.vidal@lacdr.leidenuniv.nl>
2023-11-17 15:40:24 +0100

    update settings with new path locations

--- src/QligFEP/settings/settings.py
+++ src/QligFEP/settings/settings.py
@@ -1,18 +1,20 @@
 from QligFEP import SRC
 
-ROOT_DIR = str(SRC / "QligFEP")
+ROOT_DIR = SRC / "QligFEP"
 
 # The directories to the input FF and run related input files are given here
-FF_DIR = ROOT_DIR + "FF"
-INPUT_DIR = ROOT_DIR + "INPUTS"
+FF_DIR = ROOT_DIR / "FF"
+INPUT_DIR = ROOT_DIR / "INPUTS"
 # Dicionary of locations of Q executables
-Q_DIR = str(SRC / "q6")
-# Q_DIR = {'CSB':'/home/willem/software/Q/bin/q6/',
-#          'LOCAL':'/home/willem/software/Q/bin/q6/',
-#          'SNELLIUS':'/home/wjespers/software/Q/bin/q6/'
-#          #'LOCAL':'/Users/willemjespers/Software/Q6/bin/'
-#         }
-BIN = ROOT_DIR + "bin"
+Q_DIR = SRC / "q6"
+BIN = ROOT_DIR / "bin/q6"
+
+if not BIN.exists():
+    raise FileNotFoundError(
+        f"Could not find {BIN}.\n"
+        "Please make sure you have compiled q6 by running "
+        f"`make all && make mpi` in the {Q_DIR} directory."
+    )
 
 # some example schrodinger directory
 SCHROD_DIR = '/mnt/c/Program\ Files/Schrodinger2022-2/'
@@ -20,15 +22,28 @@ SCHROD_DIR = '/mnt/c/Program\ Files/Schrodinger2022-2/'
 # quick fix to run .exe files on wsl
 EXE = '.exe'
 
+Q_PATHS = {
+    'QDYN'       : 'qdyn=' + str((BIN / 'qdynp').absolute()),
+    'QPREP'      : str(BIN / 'qprep'),
+    'QFEP'       : str(BIN / 'qfep'),
+    'QCALC'      : str(BIN / 'qcalc'),
+}
+
+CONFIGS = {
+    "ROOT_DIR" : str(ROOT_DIR),
+    "FF_DIR" : str(FF_DIR),
+    "INPUT_DIR" : str(INPUT_DIR),
+    "Q_DIR" : str(Q_DIR),
+    "BIN" : str(BIN),
+    "SCHROD_DIR" : str(SCHROD_DIR),
+    **Q_PATHS
+}
 # CLUSTER INPUTS. To add your own cluster, use the same input as below
 CSB = {'NODES'        : '1',
        'NTASKS'       : '16',
        'TIME'         : '0-12:00:00',  # d-hh:mm:ss
        'MODULES'      : 'module load gcc/6.2.0\n module load openmpi/2.1.0',
-       'QDYN'         : 'qdyn=' + Q_DIR + 'qdynp',
-       'QPREP'        : Q_DIR + 'qprep',
-       'QFEP'         : Q_DIR + 'qfep',
-       'QCALC'        : Q_DIR + 'qcalc'
+       **Q_PATHS
       }
 
 # CLUSTER INPUTS. To add your own cluster, use the same input as below
@@ -36,21 +51,15 @@ SNELLIUS = {'NODES'        : '1',
             'NTASKS'       : '16',
             'TIME'         : '0-12:00:00',  # d-hh:mm:ss
             'MODULES'      : 'module load 2021\n module load gompi/2021a',
-            'QDYN'         : 'qdyn=' + Q_DIR + 'qdynp',
-            'QPREP'        : Q_DIR + 'qprep',
-            'QFEP'         : Q_DIR + 'qfep',
-            'QCALC'        : Q_DIR + 'qcalc'
+            **Q_PATHS
       }
 
-ALICE = {'MAINDIR'      : '/home/jespersw/software/q6/',
+ALICE = {'MAINDIR'      : Q_DIR,
          'NODES'        : '1',
          'NTASKS'       : '24',
          'TIME'         : '0-3:00:00',  # d-hh:mm:ss
          'MODULES'      : 'module load OpenMPI/3.1.3-GCC-8.2.0-2.31.1',
-         'QDYN'         : 'qdyn=/home/jespersw/software/q6/bin/qdynp',
-         'QPREP'        : Q_DIR + 'qprep',
-         'QFEP'         : '/home/jespersw/software/q6/bin/qfep',
-         'QCALC'        : '/home/jespersw/software/q6/bin/qcalc'
+         **Q_PATHS
         }
 
 
@@ -58,58 +67,58 @@ HEBBE = {'NODES'      : '1',
          'NTASKS'     : '20',
          'TIME'       : '0-02:00:00',  # d-hh:mm:ss
          'MODULES'    : 'module load GCC/5.4.0-2.26\nmodule load OpenMPI/1.10.3\n', # Add a \n for every added module
-         'QDYN'       : 'qdyn=/c3se/users/jwillem/Hebbe/software/qsource/bin/qdyn5p', #fix qdyn= !!!!
-         'QFEP'       : 'qdyn=/c3se/users/jwillem/Hebbe/software/qsource/bin/qfep5', #fix qdyn= !!!!!!
-         'QPREP'      : '/home/jespers/software/q6/bin/qprep', # NOTE: change to where you are setting up, not where you are running!
-         'ACCOUNT'    : 'SNIC2018-2-3'
+         'ACCOUNT'    : 'SNIC2018-2-3',
+         **Q_PATHS
         }
 
 KEBNE = {'NODES'      : '1',
          'NTASKS'     : '28',
          'TIME'       : '0-04:00:00',  # d-hh:mm:ss
          'MODULES'    : 'module load gompi/2017b\n', # Add a \n for every added module
-         'QDYN'       : 'qdyn=/home/w/wije/pfs/software/q/bin/qdynp', #fix qdyn= !!!!!
-         'QPREP'      : '/home/jespers/software/q6/bin/qprep', # NOTE: change to where you are setting up, not where you are running!
-         'QFEP'       : '/home/w/wije/pfs/software/q/bin/qfep',
-         'QCALC'      : '/home/w/wije/pfs/software/q/bin/qcalc',
-         'ACCOUNT'    : 'SNIC2018-2-3'
+         'ACCOUNT'    : 'SNIC2018-2-3',
+         **Q_PATHS
         }
 
 STALLO = {'NODES'      : '1',
          'NTASKS'     : '20',
          'TIME'       : '0-12:00:00',  # d-hh:mm:ss
          'MODULES'    : 'module load impi/2018.1.163-iccifort-2018.1.163-GCC-6.4.0-2.28\n', # Add a \n for every added module
-         'QDYN'       : 'qdyn=/home/jespersw/software/Q6/bin/qdynp', #fix qdyn= !!!!!
-         'QPREP'      : '/home/apps/q-5.06/qprep', # NOTE: change to where you are setting up, not where you are running!
-         'ACCOUNT'    : 'nn4654K'
+         'ACCOUNT'    : 'nn4654K',
+         **Q_PATHS
         }
 
 UPPMAX = {'NODES'      : '1',
          'NTASKS'     : '20',
          'TIME'       : '0-24:00:00',  # d-hh:mm:ss
          'MODULES'    : 'gcc/9.2.0\nopenmpi/4.0.2\n', # Add a \n for every added module
-         'QDYN'       : 'qdyn=/domus/h1/willem/software/q6/bin/qdynp', #fix qdyn= !!!!!
-         'QPREP'      : '/home/apps/q-5.06/qprep', # NOTE: change to where you are setting up, not where you are running!
-         'QFEP'       : '/domus/h1/willem/software/q6/bin/qfep',
-          'ACCOUNT'    : 'snic2018-2-3'
+         'ACCOUNT'    : 'snic2018-2-3',
+         **Q_PATHS
         }
 
 TETRA  = {'NODES'      : '1',
           'NTASKS'     : '32',
           'TIME'       : '0-24:00:00',  # d-hh:mm:ss
           'MODULES'    : '\n', # Add a \n for every added module
-          'QDYN'       : 'qdyn=/home/x_wilje/Software/q6/bin/qdynp', #fix qdyn= !!!!!
-          'QPREP'      : '/home/jespers/software/q6/bin/qprep', # NOTE: change to where you are setting up, not where you are running!
-          'QFEP'       : '/home/x_wilje/Software/q6/bin/qfep',
-          'ACCOUNT'    : 'snic2019-2-1'
+          'ACCOUNT'    : 'snic2022-3-2',
+          **Q_PATHS
         }
 
 LOCAL = {'NODES'      : '',
          'NTASKS'     : '',
          'TIME'       : '',  # d-hh:mm:ss
          'MODULES'    : '\n', # Add a \n for every added module
-         'QDYN'       : 'qdyn=/Users/willemjespers/Software/Q6/bin/qdyn', #fix qdyn= !!!!!
-         'QPREP'      : '/Users/willemjespers/Software/Q6/bin/qprep', # NOTE: change to where you are setting up, not where you are running!
-         'QFEP'       : '/Users/willemjespers/Software/Q6/bin/qfep',
          'ACCOUNT'    : ''
+         **Q_PATHS
         }
+
+CLUSTER_DICT = {
+    "CSB" : CSB,
+    "SNELLIUS" : SNELLIUS,
+    "ALICE" : ALICE,
+    "HEBBE" : HEBBE,
+    "KEBNE" : KEBNE,
+    "STALLO" : STALLO,
+    "UPPMAX" : UPPMAX,
+    "TETRA" : TETRA,
+    "LOCAL" : LOCAL
+}
