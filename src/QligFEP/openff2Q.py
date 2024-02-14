5771b35b621db4ccdfbc4880ea8053ec1b09f420
David-Araripe <david.araripe17@gmail.com>
2024-02-14 10:40:31 +0100

    OpenFF2Q inherits from MoleculeIO

--- src/QligFEP/openff2Q.py
+++ src/QligFEP/openff2Q.py
@@ -1,20 +1,19 @@
 import os
 from tqdm import tqdm
 from loguru import logger
-from pathlib import Path
 import numpy as np
 
 # QligFEP modules
 from .pdb_utils import pdb_parse_out
 from .settings.settings import FF_DIR
+from .chemIO import MoleculeIO
 
 # openFF modules
-from openff.toolkit import Molecule, ForceField, Topology
+from openff.toolkit import ForceField, Topology
 from openff.interchange import Interchange
 from openff.interchange.smirnoff._create import _electrostatics
-from openff.toolkit.utils import UndefinedStereochemistryError
 
-class OpenFF2Q(object):
+class OpenFF2Q(MoleculeIO):
     """Class to process ligands and generate OpenFF parameter files for QligFEP.
 
     Attributes:
@@ -27,42 +26,15 @@ class OpenFF2Q(object):
         
         Args:
             lig: sdf file containing the ligands to be processed.
-        """        
-        self.lig = lig
+        """
+        super().__init__(lig, *args, **kwargs)
         self.FF = 'openff-2.1.0'
-        self.ff_list = []
-        self.total_charges = {}
-        self.molecules = []
-        self.lig_names = []
-        self.setup_mols_and_names()
         self.mapping = {lname:{} for lname in self.lig_names}
         self.forcefield = ForceField('openff-2.1.0.offxml')
         self.topologies, self.parameters = self.set_topologies_and_parameters()
-        self.sdf_contents = {} # store the sdf content for each entry
-        self.parse_sdf_contents() # add the sdf content to the dictionary
         self.charges_list_magnitude = {} # store charge magnitude for each ligand
         self.total_charges = {} # store the total charges
         
-    def setup_mols_and_names(self):
-        try:
-            mols = Molecule.from_file(self.lig)
-        except UndefinedStereochemistryError:
-            logger.warning('Undefined stereochemistry in the input file!! Will try to process the ligands anyway.')
-            mols = Molecule.from_file(self.lig, allow_undefined_stereo=True)
-        if not isinstance(mols, list):
-            self.single_ligand = True
-            mols = [mols]
-        else:
-            self.single_ligand = False
-        
-        if self.single_ligand:
-            self.lig_names = [self.lig.split('.')[0]]
-        else:
-            self.lig_names = [ # in some cases the name is empty so we give it a name
-                mol.name if mol.name != '' else f'lig_{idx}' for idx, mol in enumerate(mols)
-            ]
-        self.molecules = mols
-
     def set_topologies_and_parameters(self):
         topologies = {}
         parameters = {}
@@ -102,43 +74,6 @@ class OpenFF2Q(object):
             self.write_prm_Q(lname)
             self.write_PDB(lname)
             
-    def parse_sdf_contents(self):
-        """Parse the SDF content into individual entries and have them saved in a dictionary."""
-        with open(self.lig) as infile:
-            content = infile.readlines()
-            ligands = []
-            current_ligand = []
-
-            for line in content:
-                if line.strip() == '$$$$':  # End of a ligand entry
-                    ligands.append(current_ligand)
-                    current_ligand = []  # Start a new ligand entry
-                else:
-                    current_ligand.append(line.strip())
-
-        for lname, sdf_content in zip(self.lig_names, ligands):
-            self.sdf_contents.update({lname : sdf_content})
-            
-    def write_sdf_separate(self, output_dir):
-        """Function to write the separate multiple molecules within a sdf file into their own
-        .sdf, placed under `output_dir`.
-
-        Args:
-            output_dir: the directory to save the single sdf molecules.
-
-        Raises:
-            ValueError: if `output_dir` is neither a `str` or a `pathlib.Path` object.
-        """        
-        if isinstance(output_dir, str):
-            output_dir = Path(output_dir)
-        elif not isinstance(output_dir, Path):
-            raise ValueError('output_dir must be either a str or pathlib.Path object')
-        if not output_dir.exists():
-            output_dir.mkdir(exist_ok=True)
-        for idx, lname in enumerate(self.lig_names):
-            fpath = str(output_dir / f'{lname}.sdf')
-            self.molecules[idx].to_file(file_path=fpath, file_format='sdf')
-        
     def get_mapping(self, lname):
         """Get the mapping of the ligand atoms to the forcefield parameters.
 
