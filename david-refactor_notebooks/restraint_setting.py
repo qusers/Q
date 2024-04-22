9618e3608c2a157ca9973602ab81a392a3e46b8d
David-Araripe <david.araripe17@gmail.com>
2024-04-22 15:22:32 +0200

    update restraint setting method

--- david-refactor_notebooks/restraint_setting.py
+++ david-refactor_notebooks/restraint_setting.py
@@ -17,6 +17,7 @@ root_path = Path(__file__).parent
 molio = MoleculeIO(root_path / "BACE_ligands.sdf")
 molio.setup_mols_and_names()
 
+# there's an error currently because the atoms aren't correctly aligned (1A between rings)
 mol1 = molio.molecules[molio.lig_names.index("CAT-17c")]
 mol2 = molio.molecules[molio.lig_names.index("CAT-17d")]
 
@@ -47,32 +48,6 @@ atom_mapping = deepcopy(kartograf_mapping.to_dict()["componentA_to_componentB"])
 def get_surrounding_idxs(atom, mol):
     return [a.GetIdx() for a in mol.GetAtomWithIdx(atom).GetNeighbors()]
 
-def extract_sub_molecule(atom_indices, input_molecule):
-    # Validate that the atom indices list is not empty
-    if not atom_indices:
-        raise ValueError("No atom indices provided.")
-
-    # Handle the case of a single atom
-    if len(atom_indices) == 1:
-        return Chem.MolFragmentToSmiles(
-            input_molecule, atomsToUse=atom_indices, isomericSmiles=True
-        )
-
-    # Check for bonds between the atoms
-    bonds_present = all(
-        input_molecule.GetBondBetweenAtoms(idx1, idx2) is not None
-        for i, idx1 in enumerate(atom_indices)
-        for idx2 in atom_indices[i + 1 :]
-    )
-
-    # Create the sub-molecule
-    editable_molecule = RWMol(input_molecule)
-    atoms_to_remove = set(range(editable_molecule.GetNumAtoms())) - set(atom_indices)
-    for atom_idx in sorted(atoms_to_remove, reverse=True):
-        editable_molecule.RemoveAtom(atom_idx)
-    return editable_molecule.GetMol()
-
-
 def identify_and_enumerate_rings(mol):
     """Identify distinct ring structures and enumerate them."""
     ring_info = mol.GetRingInfo()
@@ -82,7 +57,7 @@ def identify_and_enumerate_rings(mol):
     return rings
 
 
-def map_rings_between_molecules(ringsA, ringsB, atom_mapping):
+def map_rings_between_molecules(ringsA, ringsB, atom_mapping): # TODO: test when you don't have respective ring
     """Map rings between molecules based on atom mapping."""
     ring_mapping = {}  # Maps ring indices in A to ring indices in B
     for idxA, atomsA in ringsA.items():
@@ -97,22 +72,27 @@ def process_rings_separately(molA, molB, atom_mapping):
     """Process each ring separately based on ring equivalency."""
     ringsA = identify_and_enumerate_rings(molA)
     ringsB = identify_and_enumerate_rings(molB)
+    rings = {}
     ring_mapping = map_rings_between_molecules(ringsA, ringsB, atom_mapping)
+    rings.update({"Ring mapping": ring_mapping})
 
-    to_remove = []
-    for idxA, idxB in ring_mapping.items():
+    print("Ring mapping: ", ring_mapping)
+    for ring_idx, (idxA, idxB) in enumerate(ring_mapping.items()):
         atomsA = ringsA[idxA]
         atomsB = ringsB[idxB]
-        substituentsA = get_enhanced_substituents(molA, atomsA)
-        substituentsB = get_enhanced_substituents(molB, atomsB)
-        for atomA_idx, subsA in substituentsA.items():
-            atomB_idx = atom_mapping.get(atomA_idx)
-            subsB = substituentsB.get(atomB_idx)
-            is_sub_equivalent = subsA == subsB
-            if not is_sub_equivalent:
-                to_remove.extend(list(substituentsB[atomA_idx][0][-1].keys()))
-
-    remove_from_mapping(atom_mapping, to_remove)
+        substituentsA = get_substituents(molA, atomsA)
+        substituentsB = get_substituents(molB, atomsB)
+        rings.update(
+            {
+                f"Ring {ring_idx}": {
+                    "ringAtomsA": atomsA,
+                    "ringAtomsB": atomsB,
+                    "substituentsA": substituentsA,  # here we have ringAtom : [substituent atoms]
+                    "substituentsB": substituentsB,
+                }
+            }
+        )
+    return rings
 
 
 def remove_from_mapping(mapping, to_remove):
@@ -162,7 +142,7 @@ def get_bond_symbol(bond):
     return ""  # Default or unrecognized bond type
 
 
-def get_enhanced_substituents(mol, ring_atoms):
+def get_substituents(mol, ring_atoms):
     substituents = {}
     visited_atoms = set(ring_atoms)  # Start with ring atoms as visited to avoid re-traversing the ring
 
@@ -178,9 +158,59 @@ def get_enhanced_substituents(mol, ring_atoms):
 
     return substituents
 
+def extract_sub_molecule(atom_indices, input_molecule):
+    """Helper function to extract a sub molecule from an input molecule based on a set of
+    atom indices.
+
+    Args:
+        atom_indices: atom indices to use for the extracted submolecule.
+        input_molecule: molecule from which to substract the submolecule.
+
+    Returns:
+        rdkit.Chem.Mol: the extracted submolecule.
+    """    
+    # Validate that the atom indices list is not empty
+    if not atom_indices:
+        raise ValueError("No atom indices provided.")
+
+    # Handle the case of a single atom
+    if len(atom_indices) == 1:
+        return Chem.MolFragmentToSmiles(
+            input_molecule, atomsToUse=atom_indices, isomericSmiles=True
+        )
+
+    # Create the sub-molecule
+    editable_molecule = RWMol(input_molecule)
+    atoms_to_remove = set(range(editable_molecule.GetNumAtoms())) - set(atom_indices)
+    for atom_idx in sorted(atoms_to_remove, reverse=True):
+        editable_molecule.RemoveAtom(atom_idx)
+    return editable_molecule.GetMol()
+
+def submolecule_to_img(sub):
+    Chem.rdDepictor.Compute2DCoords(sub)
+    return Draw.MolToImage(sub, size=(300, 300))
+
+ring_restraint_struct = process_rings_separately(
+    molA.to_rdkit(), molB.to_rdkit(), atom_mapping
+)
+# print("Updated mapping:", atom_mapping)
 
 
-# Execution
-process_rings_separately(molA.to_rdkit(), molB.to_rdkit(), atom_mapping)
-
-print("Updated mapping:", atom_mapping)
+for ringA, ringB in ring_restraint_struct["Ring mapping"].items():
+    subsA = ring_restraint_struct[f"Ring {ringA}"]["substituentsA"]
+    subsB = ring_restraint_struct[f"Ring {ringA}"]["substituentsB"]
+    print(f"For Ring {ringA}:")
+    ringA = extract_sub_molecule(list(subsA.keys()), molA.to_rdkit())
+    ringB = extract_sub_molecule(list(subsB.keys()), molB.to_rdkit())
+    img = submolecule_to_img([ringA, ringB])
+    # display(img) # for the jupyter notebook
+    print("for the substituents:")
+    for key in subsA.keys():
+        keyB = atom_mapping[key]
+        print(f"from : Atom {key} -> Atom {keyB}")
+        atomsA = [key] + subsA[key]
+        atomsB = [keyB] + subsB[keyB]
+        subA = extract_sub_molecule(atomsA, molA.to_rdkit())
+        subB = extract_sub_molecule(atomsB, molB.to_rdkit())
+        img = submolecule_to_img([subA, subB])
+        # display(img)
