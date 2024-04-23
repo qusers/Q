31b75cf82671de120b7673c297da3a0f84d3ffe1
David-Araripe <david.araripe17@gmail.com>
2024-04-23 11:59:19 +0200

    finish qprep cli implementation

--- src/QligFEP/CLI/qprep_cli.py
+++ src/QligFEP/CLI/qprep_cli.py
@@ -4,10 +4,9 @@ import argparse
 from pathlib import Path
 from typing import Optional
 
-from env.settings import FF_DIR
-from ..settings.settings import CONFIGS
 from ..IO import run_command
 from ..logger import logger
+from ..settings.settings import CONFIGS
 
 # NOTE: cysbonds will have \n after each bond -> `maketop MKC_p` is in a different line
 qprep_inp_content = """rl {ff_lib_path}
@@ -40,7 +39,11 @@ def parse_arguments() -> argparse.Namespace:
         )
     )
     parser.add_argument(
-        "-pdb", "--pdb_file", dest='pdb_file', required=True, help="input protein to run qprep"
+        "-pdb",
+        "--pdb_file",
+        dest="pdb_file",
+        required=True,
+        help="input protein to run qprep",
     )
     parser.add_argument(
         "-FF",
@@ -78,25 +81,29 @@ def parse_arguments() -> argparse.Namespace:
             "(participating in the Cys bond) connected by `_` and with different bonds "
             "separated by `,` as in: `atom1_atom2,atom3_atom4`"
         ),
-        type=str
+        type=str,
     )
     return parser.parse_args()
 
+
 def main(args: Optional[argparse.Namespace] = None, **kwargs) -> None:
-    """Either 
+    """Either
 
     Args:
         args: _description_. Defaults to None.
     """
     cwd = Path.cwd()
-    qprep_inp_path = cwd / 'qprep.inp'
-    qprep_path = CONFIGS['qprep']
-    ff_lib_path = str(Path(FF_DIR) / f'{args.FF}.lib')
-    ff_prm_path = str(Path(FF_DIR) / f'{args.FF}.prm')
-    if args.cysbond != "":
-        cysbonds = args.cysbond.split(',')
-        cysbonds = '\n'.join([f'addbond {b.split('_')[0]} {b.split('_')[1]} y' for b in cysbonds])
-    
+    qprep_inp_path = cwd / "qprep.inp"
+    qprep_path = CONFIGS["QPREP"]
+    ff_lib_path = str(Path(CONFIGS["FF_DIR"]) / f"{args.FF}.lib")
+    ff_prm_path = str(Path(CONFIGS["FF_DIR"]) / f"{args.FF}.prm")
+    cysbonds = args.cysbond
+    if cysbonds != "":
+        cysbonds = cysbonds.split(",")
+        cysbonds = "\n".join(
+            [f"addbond {b.split('_')[0]} {b.split('_')[1]} y" for b in cysbonds]
+        )
+
     # format the cysbonds = addbond at1 at2 y
     if args is not None:
         param_dict = {
@@ -109,18 +116,23 @@ def main(args: Optional[argparse.Namespace] = None, **kwargs) -> None:
         }
     else:
         param_dict = {}
-    
+
     # write qprep.inp with the formatted qprep_inp_content using Path
     if qprep_inp_path.exists():
-        logger.warning("qprep.inp already exists!! Skipping qprep.inp file generation...")
+        logger.warning(
+            "qprep.inp already exists!! Skipping qprep.inp file generation..."
+        )
     else:
-        with qprep_inp_path.open('w') as qprep_inp_f:
+        with qprep_inp_path.open("w") as qprep_inp_f:
             qprep_inp_f.write(qprep_inp_content.format(**param_dict))
-    
-    options = ' < qprep.inp > qprep.out'
-    run_command(qprep_path, options, string = True)
-    
+
+    options = " < qprep.inp > qprep.out"
+    run_command(qprep_path, options, string=True)
+    logger.info(
+        "qprep run finished. Check the output `qprep.out` for more information."
+    )
+
+
 def main_exe():
     args = parse_arguments()
     main(args)
-    
