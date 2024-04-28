c4da4469ec52f5ce97bc44a081c1d84a69b4fe15
David-Araripe <david.araripe17@gmail.com>
2024-04-28 20:53:04 +0200

    add hydrogens to aligned mols & format code

--- src/QligFEP/lig_aligner.py
+++ src/QligFEP/lig_aligner.py
@@ -10,6 +10,7 @@ from . import SRC
 from .chemIO import MoleculeIO
 from .logger import logger
 
+
 class LigandAligner:
     def __init__(self, molio: MoleculeIO, n_threads: int = 1):
         """initalize the ligand aligner class from a MoleculeIO object.
@@ -20,18 +21,21 @@ class LigandAligner:
 
         Raises:
             FileNotFoundError: If the kcombu executable is not found.
-        """        
+        """
         self.molecules = [deepcopy(m.to_rdkit()) for m in molio.molecules]
         self.lig_names = molio.lig_names
-        self.kcombu_exe = str(SRC  / "kcombu/fkcombu")
+        self.kcombu_exe = str(SRC / "kcombu/fkcombu")
         if not Path(self.kcombu_exe).exists():
-            raise FileNotFoundError(f"Could not find kcombu executable at {self.kcombu_exe} make sure it is installed.")
+            raise FileNotFoundError(
+                f"Could not find kcombu executable at {self.kcombu_exe} make sure it is installed."
+            )
         molio.parse_sdf_contents()
         molio.write_sdf_separate("to_align_ligands")
-        
+
     def _transfer_sdf_metadata(self, original_file, aligned_file):
         """
-        Copies metadata from the original SDF file to the aligned SDF file.
+        Copies metadata from the original SDF file to the aligned SDF file and adds
+        hydrogens to the aligned molecules.
 
         Args:
         original_file (str or Path): Path to the original SDF file.
@@ -49,7 +53,7 @@ class LigandAligner:
                 for prop_name in original_mol.GetPropNames():
                     prop_value = original_mol.GetProp(prop_name)
                     aligned_mol.SetProp(prop_name, prop_value)
-
+                    aligned_mol = Chem.AddHs(aligned_mol, addCoords=True)
         aligned_writer = Chem.SDWriter(str(aligned_file))
         for mol in aligned_mols:
             aligned_writer.write(aligned_mol)
@@ -76,7 +80,7 @@ class LigandAligner:
                 else:
                     logger.error(f"Aligned file {aligned_sdf} not found.")
 
-    def kcombu_align(self, reference:str) -> None:
+    def kcombu_align(self, reference: str) -> None:
         """Aligns the ligands to a reference ligand using kcombu.
 
         Args:
@@ -86,34 +90,34 @@ class LigandAligner:
         ligpath = cwd / "to_align_ligands"
         ref_sdf = ligpath / f"{reference}.sdf"
         suffix = f"_{reference}_aligned"
-        
+
         self.reference = ref_sdf
-        to_align_sdfs = [ligpath / f"{name}.sdf" for name in self.lig_names if name != reference]
-        
+        to_align_sdfs = [
+            ligpath / f"{name}.sdf" for name in self.lig_names if name != reference
+        ]
+
         commands = []
-        logger.info(f'Aligning ligands to ref `{reference}` with kcombu...')
+        logger.info(f"Aligning ligands to ref `{reference}` with kcombu...")
         for lig in to_align_sdfs:
-            kcombu_options = (
-                f"-T {lig} -R {ref_sdf} -osdfT {lig.with_stem(lig.stem + suffix)} -E 'V'"
-            )
+            kcombu_options = f"-T {lig} -R {ref_sdf} -osdfT {lig.with_stem(lig.stem + suffix)} -E 'V'"
             commands.append(f"{self.kcombu_exe} {kcombu_options}")
-        
+
         commands = tqdm(commands)
         partial_func = partial(subprocess.run, capture_output=True, text=True)
-        with parallel_config(backend='threading', n_jobs=2):
+        with parallel_config(backend="threading", n_jobs=2):
             Parallel()(delayed(partial_func)(cmd.split()) for cmd in commands)
         self._update_aligned_sdf_files(self.lig_names, reference, ligpath)
-            
-    def aligned_ligands_to_sigle_file(self,output_name: str, align_dir= None) -> None:
+
+    def aligned_ligands_to_sigle_file(self, output_name: str, align_dir=None) -> None:
         """Writes the aligned ligands to a single file.
 
         Args:
             align_dir: _description_
             output_name: _description_
-        """        
+        """
         if align_dir is None:
             align_dir = Path.cwd() / "to_align_ligands"
-        else: 
+        else:
             align_dir = Path(align_dir)
         sdf_files = [lig for lig in align_dir.glob("*_aligned.sdf")] + [self.reference]
 
@@ -129,6 +133,5 @@ class LigandAligner:
         for mol in molecules:
             writer.write(mol)
         writer.close()
-        
+
         logger.info(f"Aligned ligands written to {output_name}")
-        
