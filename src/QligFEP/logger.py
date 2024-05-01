c8747b130a882027ff36739432bad5465d9fdbe6
David-Araripe <david.araripe17@gmail.com>
2024-05-01 09:05:19 +0200

    make logger always take the level as .upper()

--- src/QligFEP/logger.py
+++ src/QligFEP/logger.py
@@ -1,10 +1,14 @@
 # log_config.py
-from loguru import logger
 import sys
 
+from loguru import logger
+
+
 def setup_logger(level="INFO"):
+    level = level.upper()
     colorful_format = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
     logger.remove()
     logger.add(sys.stderr, format=colorful_format, level=level, colorize=True)
+
 
 setup_logger()
