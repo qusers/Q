928e25574116ca478991cd79fb8300e18c5cad61
David-Araripe <david.araripe17@gmail.com>
2024-06-12 12:06:20 +0200

    Support using NAGL on the qparams command line

--- src/QligFEP/CLI/qparams_cli.py
+++ src/QligFEP/CLI/qparams_cli.py
@@ -2,6 +2,7 @@
 
 import argparse
 
+from ..logger import setup_logger
 from ..openff2Q import OpenFF2Q
 
 # TODO: implement the other parameter writing functionalities
@@ -45,12 +46,33 @@ def parse_arguments() -> argparse.Namespace:
         ),
         type=int,
     )
+    parser.add_argument(
+        "-nagl",
+        "--use_nagl",
+        dest="nagl",
+        action="store_true",
+        help=(
+            "Use the NAGL method to calculate the charges of the molecules using OpenFF. "
+            "This method is faster than the default one, but it does not cover all the "
+            "possible cases. Defaults to False."
+        ),
+    )
+    parser.add_argument(
+        "-log",
+        "--log-level",
+        dest="log",
+        required=False,
+        default="info",
+        help="Set the log level for the logger. Defaults to `info`.",
+        choices=["trace", "debug", "info", "warning", "error", "critical"],
+    )
     return parser.parse_args()
 
 
 def main(args: argparse.Namespace) -> None:
+    setup_logger(level=args.log)
     if args.forcefield == "OpenFF":
-        openff2q = OpenFF2Q(args.input, n_jobs=args.parallel)
+        openff2q = OpenFF2Q(args.input, nagl=args.nagl, n_jobs=args.parallel)
         openff2q.process_ligands()
     else:
         raise NotImplementedError("Forcefield not supported through this CLI yet")
