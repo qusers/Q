4a402590761f38e8283b433a0936ef3c8a2690ee
David-Araripe <david.araripe17@gmail.com>
2024-12-20 12:37:09 +0100

    submit.sh support submission of single jobs from the job arrary through positional arguments

--- src/QligFEP/INPUTS/FEP_submit.sh
+++ src/QligFEP/INPUTS/FEP_submit.sh
@@ -4,4 +4,12 @@ workdir="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 echo $wrkdir
 inputfiles=$workdir/inputfiles
 submitfile=$inputfiles/RUNFILE
-sbatch $submitfile
+
+if [ $# -eq 0 ]; then # if no arguments provided, submit the whole job array
+    echo "No arguments provided - submitting all arrays (1-10)"
+    sbatch $submitfile
+else
+    array_indices=$(echo "$@" | tr ' ' ',') # convert space-separated arguments to comma-separated
+    echo "Submitting job array indexes: $array_indices"
+    sbatch --array=$array_indices $submitfile
+fi
