5417d78adb01dab3dc3d3fe233086433fac1b3ce
shen.guo <g.shen@rug.nl>
2026-08-13 15:03:59 +0200

    finish cpu lincs

---
+++ src/core/cpu/include/cpu_lincs.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "constraint_force.h"
+
+struct LincsSettings {
+    int expansion_order = 8;
+    int minimum_rotation_iterations = 2;
+    int maximum_rotation_iterations = 8;
+    double accuracy_tolerance = 1e-6;
+};
+
+struct LincsData {
+    std::vector<double> bond_inv_sqrt_inv_mass_sum;
+    std::vector<std::vector<int>> bond_graph;
+    std::vector<std::vector<double>> normalized_c;
+};
+
+class CpuLincs : public ConstraintForce {
+   public:
+    explicit CpuLincs(LincsSettings lincs_settings = {}) : lincs_settings_(lincs_settings) {
+    }
+
+    void apply(Context& ctx, HostDeviceBuffer<coord_t>& xcoords) override;
+    void initial_constraint(Context& ctx) override;
+
+   protected:
+    void init_backend(Context& ctx) override;
+
+   private:
+    void apply_to(Context& ctx, coord_t* coords, const coord_t* xcoords);
+    LincsData lincs_data_;
+    LincsSettings lincs_settings_;
+
+    int get_sign(const int a, const ConstraintBond& bond);
+
+    void solve(Context& ctx, std::vector<double> errors, coord_t* coords, const coord_t* xcoords);
+    bool check_ready(const coord_t* coords);
+};
